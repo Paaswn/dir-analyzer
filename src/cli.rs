261138316ae71599fa5c0efeefee2841861af0c2
Paaswn <patsawin12@gmail.com>
2026-02-08 16:37:15 +0700

    Rename package to dira and fix cli argument handling
    The package name has been changed from `dir-analyzer` to `dira`.
    Additionally, the CLI argument handling has been improved to ensure a
    command is always provided.

--- src/cli.rs
+++ src/cli.rs
@@ -4,9 +4,10 @@ use clap::{Parser, Subcommand};
 use std::path::PathBuf;
 
 #[derive(Parser)]
+#[command(arg_required_else_help = true)]
 struct Cli {
     #[command(subcommand)]
-    command: Option<Commands>,
+    command: Commands,
 }
 
 #[derive(Subcommand)]
@@ -32,29 +33,24 @@ pub enum Commands {
 pub fn parsing() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
     match &cli.command {
-        Some(command) => match command {
-            Commands::Scan { path } => {
-                if let Some(path) = path {
-                    println!("{:?}", path);
-                }
+        Commands::Scan { path } => {
+            if let Some(path) = path {
+                println!("{:?}", path);
             }
-            Commands::Size { path, top } => {
-                if let Some(path) = path {
-                    size::print_sizes(&path, *top)?;
-                } else {
-                    size::print_sizes(&PathBuf::from("."), *top)?;
-                }
+        }
+        Commands::Size { path, top } => {
+            if let Some(path) = path {
+                size::print_sizes(&path, *top)?;
+            } else {
+                size::print_sizes(&PathBuf::from("."), *top)?;
             }
-            Commands::Loc { path, top } => {
-                if let Some(path) = path {
-                    loc::print_loc(path, *top)?;
-                } else {
-                    loc::print_loc(&PathBuf::from("."), *top)?;
-                }
+        }
+        Commands::Loc { path, top } => {
+            if let Some(path) = path {
+                loc::print_loc(path, *top)?;
+            } else {
+                loc::print_loc(&PathBuf::from("."), *top)?;
             }
-        },
-        None => {
-            eprintln!("Error: No command provided")
         }
     }
     Ok(())
